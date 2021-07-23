613ff87fcf206376b5aa2814aaa4a30e3857b22d
Josman Pérez <josman.perez@mongodb.com>
2021-07-23 09:31:18 +0200

    * Added frontend

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import path from 'path';
 
 const app = express();
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(express.json());
 
 const withDB = async (operations, res) => {
@@ -49,5 +51,9 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
         res.status(200).json(updateArticleInfo);
     }, res);
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
 
 app.listen(8000, () => console.log("listening on port 8000"));
